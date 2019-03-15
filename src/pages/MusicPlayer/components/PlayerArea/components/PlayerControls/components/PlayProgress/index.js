3ec4b5697b268632dba9d4a54a1b5b2053ae3eac
UrsulaRodgers <ursula.r.rodgers@gmail.com>
2019-03-15 15:14:42 +0000

    updates to readme, PlayerControls and theme

--- src/pages/MusicPlayer/components/PlayerArea/components/PlayerControls/components/PlayProgress/index.js
+++ src/pages/MusicPlayer/components/PlayerArea/components/PlayerControls/components/PlayProgress/index.js
@@ -2,20 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Grid } from "../../../../../../../../components_shared";
 
-const renderTrackLength = selectedTrack => {
-  return selectedTrack ? (
-    <p className="playprogress_text">{selectedTrack.length}</p>
-  ) : (
-    <p className="playprogress_text">0:00</p>
-  );
-};
-
 const PlayProgress = props => {
   const { selectedTrack } = props;
   return (
     <Grid className="playercontrols_play_progress" data-testid="track-progress">
       <p className="playprogress_text">0:00 /</p>
-      &nbsp;{renderTrackLength(selectedTrack)}
+      &nbsp; <p className="playprogress_text">{selectedTrack.length}</p>
     </Grid>
   );
 };
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,7 +1,7 @@
 const palette = {
   palette01: "#d61515",
   palette02: "#a39e9e",
-  palette03: "#02209A"
+  palette03: "#4A547E"
 };
 
 export const colors = {
